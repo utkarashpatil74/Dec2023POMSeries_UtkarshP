708b9d1bdae8977aee1bfb845d04925473754c76
Utkarsh Patil <utkarshpatil26.com>
2024-04-25 00:28:37 +0530

    changes in appconstants.java file

--- src/main/java/com/qa/opencart/constants/AppConstants.java
+++ src/main/java/com/qa/opencart/constants/AppConstants.java
@@ -2,7 +2,7 @@ package com.qa.opencart.constants;
 
 public class AppConstants {
 	
-	public static final String LOGIN_PAGE_TITLE = "Account Login11";
+	public static final String LOGIN_PAGE_TITLE = "Account Login";
 
 	public static final String ACCOUNTS_PAGE_TITLE = "My Account";
 
