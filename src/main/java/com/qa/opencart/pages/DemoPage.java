12206a11df6dc65cd3102982fccabb9ade400b30
Utkarsh Patil <utkarshpatil26.com>
2024-04-25 16:19:19 +0530

    added the by locator and actions page

--- src/main/java/com/qa/opencart/pages/DemoPage.java
+++ src/main/java/com/qa/opencart/pages/DemoPage.java
@@ -5,9 +5,9 @@ public class DemoPage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
+		System.out.println("By locator");
 		System.out.println("Hi this is demo page !");
-
+		System.out.println("Actions class");
 	}
 
 }
